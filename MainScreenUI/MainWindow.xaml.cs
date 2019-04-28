cc21c8312a3f81e9f6a07ce9e0dcaddca49eb61f
ATIQ <atik2626@gmail.com>
2019-04-29 06:13:05 +0800

    Importing Project

---
+++ MainScreenUI/MainWindow.xaml.cs
@@ -0,0 +1,47 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace MainScreenUI
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+        }
+
+        private void ButtonLogOut_Click(object sender, RoutedEventArgs e)
+        {
+            Application.Current.Shutdown();
+        }
+
+        private void GridCornerButtons_MouseDown(object sender, MouseButtonEventArgs e)
+        {
+            DragMove();
+        }
+
+        private void BtnClickProfile(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new UserProfile();
+        }
+        private void BtnClickExercise(object sender, RoutedEventArgs e)
+        {
+            Main.Content = new Exercise();
+        }
+    }
+}
