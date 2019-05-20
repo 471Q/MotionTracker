804aac18c3d10a7091b8ad83252914944d406141
CalvinLee96 <100081898@students.swinburne.edu.my>
2019-05-20 14:56:56 +0800

    1. Fixed System.Drawing Bitmap dependencies error. Please Install .NET 4.8 SDK (not client/runtime version) 2. Exercise Menu GUI is complete. The canvas is colored red for visual indicator

--- MainScreenUI/Properties/Resources.Designer.cs
+++ MainScreenUI/Properties/Resources.Designer.cs
@@ -59,15 +59,5 @@ namespace MainScreenUI.Properties {
                 resourceCulture = value;
             }
         }
-        
-        /// <summary>
-        ///   Looks up a localized resource of type System.Drawing.Bitmap.
-        /// </summary>
-        internal static System.Drawing.Bitmap buyer_persona_image1 {
-            get {
-                object obj = ResourceManager.GetObject("buyer_persona_image1", resourceCulture);
-                return ((System.Drawing.Bitmap)(obj));
-            }
-        }
     }
 }
